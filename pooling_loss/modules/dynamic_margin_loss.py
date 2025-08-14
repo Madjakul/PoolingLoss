fix: add different weighting method to dynamic losses

pooling_loss/modules/dynamic_margin_loss.py
@@ -47,7 +47,16 @@ class DynamicMarginLoss(BaseLoss):
         neg_dists = all_dists[targets, targets + batch_size]
 
         positive_loss = pos_dists.pow(2).sum()
-        dynamic_margin = self.cfg.execution.margin / q_mask_sum.float()
+        if self.cfg.execution.weighting == "log":
+            dynamic_margin = self.cfg.execution.margin / torch.log(
+                q_mask_sum.float() + 1
+            )
+        elif self.cfg.execution.weighting == "sqrt":
+            dynamic_margin = self.cfg.execution.margin / torch.sqrt(
+                q_mask_sum.float() + 1e-8
+            )
+        else:
+            dynamic_margin = self.cfg.execution.margin / q_mask_sum.float()
         negative_loss = F.relu(dynamic_margin - neg_dists).pow(2).sum()
         loss = 0.5 * (positive_loss + negative_loss)
 

pooling_loss/modules/dynamic_triplet_loss.py
@@ -47,8 +47,17 @@ class DynamicTripletLoss(BaseLoss):
         pos_dists = all_dists[targets, targets]
         negs = all_scores[targets, targets + batch_size]
         neg_dists = all_dists[targets, targets + batch_size]
+        if self.cfg.execution.weighting == "log":
+            dynamic_margin = self.cfg.execution.margin / torch.log(
+                q_mask_sum.float() + 1
+            )
+        elif self.cfg.execution.weighting == "sqrt":
+            dynamic_margin = self.cfg.execution.margin / torch.sqrt(
+                q_mask_sum.float() + 1e-8
+            )
+        else:
+            dynamic_margin = self.cfg.execution.margin / q_mask_sum.float()
 
-        dynamic_margin = self.cfg.execution.margin / q_mask_sum.float()  # shape: (B,)
         loss = F.relu(pos_dists - neg_dists + dynamic_margin).mean()
 
         return {